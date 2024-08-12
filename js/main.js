5a3e8af609c34399cff155d78dfb9e3d1092e544
sherif altiby <sherifahmed852002@gmail.com>
2024-08-12 14:08:39 +0300

    add sections

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const showResponsiveMenuBtn = document.querySelector('.nav__content .bar__icon'),
        responsiveMenu = document.querySelector('.responsive__menu'),
        overlay = document.querySelector('.overlay'),
-       allLinks = document.querySelectorAll('.nav__content .nav__links a')
+       allLinks = document.querySelectorAll('.nav__content .nav__links a'),
+       allLinksInResponsiveMenu = document.querySelectorAll('.responsive__menu .nav__links a'),
+       languageMenu = document.querySelector('nav .app__language__menu'),
+       languageBtn = document.querySelector('nav .app__language'),
+       languageMenuInResponsive = document.querySelector(".responsive__menu .app__language__menu"),
+       languageBtnResponsive = document.querySelector(".responsive__menu .app__language")
 
 
     //    show responsive menu
@@ -29,4 +34,29 @@ const showResponsiveMenuBtn = document.querySelector('.nav__content .bar__icon')
 
             link.classList.add('active')
         })
+    })
+
+    // active link in responsive menu
+
+    allLinksInResponsiveMenu.forEach((link) => {
+        link.addEventListener("click", () => {
+            allLinksInResponsiveMenu.forEach((a) => {
+                a.classList.remove("active")
+            })
+
+            link.classList.add('active');
+            overlay.classList.remove("show");
+            responsiveMenu.classList.remove("show");
+        })
+    })
+
+    // shwo langauge menu
+
+    languageBtn.addEventListener('click', () => {
+        languageMenu.classList.toggle("show")
+    })
+
+
+    languageBtnResponsive.addEventListener('click', () => {
+        languageMenuInResponsive.classList.toggle("show")
     })
